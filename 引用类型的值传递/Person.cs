1667199bab24483445a1fb82dbd369e3a8561b57
扬 童 <452949529@qq.com>
2018-08-06 16:56:45 +0800

    引用类型的值传递

---
+++ 引用类型的值传递/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 引用类型的值传递
+{
+    class Person
+    {
+        private string _name;
+        public string Name { get => _name; set => _name = value; }
+    }
+}
---
+++ 引用类型的值传递/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 引用类型的值传递
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Name = "张三";
+            Person p2 = p1;
+            p2.Name = "李四";
+            Console.WriteLine(p1.Name);
+            Console.WriteLine(object.ReferenceEquals(p1, p2));
+            Console.ReadKey();
+        }
+    }
+}
