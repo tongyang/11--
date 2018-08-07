62e239cd3b44dbe77223de51faa637285a47a0d0
扬 童 <452949529@qq.com>
2018-08-07 10:52:11 +0800

    数据传输之序列化与反序列化

---
+++ 序列号与反序列号/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace 序列号与反序列号
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            //p.Name = "童扬";
+            //p.Age = 30;
+            //string filePath = @"C:\Users\45294\Desktop\111.txt ";
+            //char[] sp = new char[] { ' ' };
+            //string[] strs = filePath.Split(sp, StringSplitOptions.RemoveEmptyEntries);
+            //filePath = strs.ToString();
+            //using (FileStream fsWriter = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write))
+            //{
+            //    BinaryFormatter bf = new BinaryFormatter();
+            //    bf.Serialize(fsWriter, p);
+            //}
+            //Console.WriteLine("序列化成功");
+            //Console.ReadKey();
+
+            using (FileStream fsRead = new FileStream(@"C:\Users\45294\Desktop\111.txt", FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                p = (Person)bf.Deserialize(fsRead);
+            }
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Age);
+            Console.ReadKey();
+        }
+    }
+
+    [Serializable]
+    class Person
+    {
+        private string _name;
+
+        public string Name { get => _name; set => _name = value; }
+        public int Age { get => _age; set => _age = value; }
+
+        private int _age;
+    }
+}
--- 引用类型的值传递/Program.cs
+++ 引用类型的值传递/Program.cs
@@ -16,7 +16,16 @@ namespace 引用类型的值传递
             p2.Name = "李四";
             Console.WriteLine(p1.Name);
             Console.WriteLine(object.ReferenceEquals(p1, p2));
+
+            int number = 10;
+            TestTwo(ref number);
+            Console.WriteLine(number);
             Console.ReadKey();
+        }
+
+        public static void TestTwo(ref int n) // ref关键字把单纯的值传递变为引用传递，即将变量的地址当做参数进行传递
+        {
+            n++;
         }
     }
 }
